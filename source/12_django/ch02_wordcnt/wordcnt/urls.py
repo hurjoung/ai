f1ff045d37cc0cfeab376a54b8325b16f73e1660
hurjoung <hj190@naver.com>
2026-01-15 11:38:37 +0900

    20260115_django

---
+++ source/12_django/ch02_wordcnt/wordcnt/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from wordcnt.views import wordinput
+
+app_name = "wordcnt"
+
+urlpatterns = [
+  path("", wordinput, name="wordinput"), # /wordcnt/ 단어입력 받는 페이지
+  # path("about/", about, name="about"), # /wordcnt/about/ 도움말
+  # path("result/", result, name="result"), # /wordcnt/result/ 단어입력결과
+]
